b50acd769429cbe25b9ab5eede9504b859ef7aa2
Ali DEMIR <ademir@iut.univ-paris8.fr>
2026-05-19 14:38:44 +0200

    Modif du nom du fichier Controller et avandcement dans la carte

--- src/main/java/universite_paris8/iut/ademir/demo1/Controller.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Controller.java
@@ -1,17 +1,13 @@
 package universite_paris8.iut.ademir.demo1;
 
-import com.sun.tools.javac.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.ImageCursor;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.TilePane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class Controller implements Initializable {
 
     @FXML
     private TilePane grilleCarte;
