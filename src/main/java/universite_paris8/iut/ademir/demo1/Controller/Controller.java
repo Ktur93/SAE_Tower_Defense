36afad64659800ed94ad59cee903c5f5e78b1f49
Mathis AURENSAN <maurensan@a205-09.iut.univ-paris8.fr>
2026-05-19 14:53:42 +0200

    Ajout de CarteVue modif de Carte,Controller,jeu.fxml et ajout des tuiles

---
+++ src/main/java/universite_paris8/iut/ademir/demo1/Controller/Controller.java
@@ -0,0 +1,30 @@
+package universite_paris8.iut.ademir.demo1.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.TilePane;
+import universite_paris8.iut.ademir.demo1.Modele.Carte;
+import universite_paris8.iut.ademir.demo1.Vue.CarteVue;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private TilePane paneCarte;
+
+    private Carte carte;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        carte = new Carte();
+        carte.Remplir();
+        CarteVue carteVue = new CarteVue(carte, paneCarte);
+        carteVue.dessinerCarte();
+
+    }
+}
