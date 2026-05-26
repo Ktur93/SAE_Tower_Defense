11469aa301b49632371fac0b577ebb7a90816725
Ali DEMIR <ademir@iut.univ-paris8.fr>
2026-05-26 12:25:26 +0200

    Modif de la classe Monstre et de ses sous classes

--- src/main/java/universite_paris8/iut/ademir/demo1/Modele/Monstres/Monstre.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Modele/Monstres/Monstre.java
@@ -1,19 +1,60 @@
 package universite_paris8.iut.ademir.demo1.Modele.Monstres;
 
-public abstract class Monstre {
-    private int pv;
+import universite_paris8.iut.ademir.demo1.Modele.Cartes.Position;
+import java.util.ArrayList;
 
-    public Monstre(int pv){
+public abstract class Monstre {
+
+    private int pv;
+    private int vitesse;
+    private int recompense;
+
+    private ArrayList<Position> chemin;
+    private int indiceChemin;
+
+    public Monstre(int pv, int vitesse, int recompense, ArrayList<Position> chemin) {
         this.pv = pv;
+        this.vitesse = vitesse;
+        this.recompense = recompense;
+        this.chemin = chemin;
+        this.indiceChemin = 0;
+    }
+
+    public void avancer() {
+        if (!estArrive()) {
+            indiceChemin++;
+        }
+    }
+
+    public boolean estArrive() {
+        return indiceChemin >= chemin.size() - 1;
+    }
+
+    public Position getPosition() {
+        return chemin.get(indiceChemin);
     }
 
     public int getPv() {
-        return this.pv;
+        return pv;
     }
 
     public void setPv(int pv) {
         this.pv = pv;
     }
 
+    public int getVitesse() {
+        return vitesse;
+    }
 
+    public int getRecompense() {
+        return recompense;
+    }
+
+    public void recevoirDegats(int degats) {
+        pv -= degats;
+    }
+
+    public boolean estMort() {
+        return pv <= 0;
+    }
 }
--- src/main/java/universite_paris8/iut/ademir/demo1/Modele/Monstres/Squelette.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Modele/Monstres/Squelette.java
@@ -1,7 +1,11 @@
 package universite_paris8.iut.ademir.demo1.Modele.Monstres;
 
-public class Squelette extends Monstre{
-    public Squelette(int pv){
-        super(pv);
+import universite_paris8.iut.ademir.demo1.Modele.Cartes.Position;
+import java.util.ArrayList;
+
+public class Squelette extends Monstre {
+
+    public Squelette(ArrayList<Position> chemin) {
+        super(90, 1, 15, chemin);
     }
 }
