677867d3540bde8aebbc813e16bb3cef1f355332
Ali DEMIR <ademir@iut.univ-paris8.fr>
2026-05-26 12:36:40 +0200

    modif de controller,partie,jeu.fxml

--- src/main/java/universite_paris8/iut/ademir/demo1/Controller/Controller.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Controller/Controller.java
@@ -1,94 +1,130 @@
 package universite_paris8.iut.ademir.demo1.Controller;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
-import javafx.animation.AnimationTimer;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+
 import universite_paris8.iut.ademir.demo1.Main;
-import universite_paris8.iut.ademir.demo1.Modele.Jeu.Partie;
+import universite_paris8.iut.ademir.demo1.Modele.Algorithmes.BFS;
 import universite_paris8.iut.ademir.demo1.Modele.Cartes.Carte;
+import universite_paris8.iut.ademir.demo1.Modele.Cartes.Position;
 import universite_paris8.iut.ademir.demo1.Modele.Jeu.Partie;
 import universite_paris8.iut.ademir.demo1.Modele.Monstres.*;
 import universite_paris8.iut.ademir.demo1.Vue.CarteVue;
-import universite_paris8.iut.ademir.demo1.Modele.Algorithmes.BFS;
-import universite_paris8.iut.ademir.demo1.Modele.Cartes.Position;
+
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private static final int TAILLE_TUILE = 64;
+
     @FXML
     private TilePane paneCarte;
-    private Carte carte;
-    private Partie partie;
+
+    @FXML
     private Pane paneSprites;
 
-
+    private Carte carte;
+    private Partie partie;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         carte = new Carte();
         carte.Remplir();
+
         CarteVue carteVue = new CarteVue(carte, paneCarte);
         carteVue.dessinerCarte();
-        partie = new Partie();
+
+        Position depart = new Position(0, 7);
+        Position arrivee = carte.trouverArrivee();
+
+        if (arrivee == null) {
+            System.out.println("Erreur : aucune arrivée trouvée dans la carte.");
+            return;
+        }
+
+        BFS bfs = new BFS(carte, depart);
+        ArrayList<Position> chemin = bfs.cheminDeSourceVersCible(arrivee);
+
+        System.out.println("Départ : " + depart);
+        System.out.println("Arrivée : " + arrivee);
+        System.out.println("Chemin BFS : " + chemin);
+
+        partie = new Partie(chemin);
         partie.ajouterZombie();
 
         AnimationTimer gameLoop = new AnimationTimer() {
+            private long dernierDeplacement = 0;
+
             @Override
             public void handle(long now) {
-                partie.MettreAJour();
-                afficherMonstres();
+                if (now - dernierDeplacement > 500_000_000) {
+                    partie.mettreAJour();
+                    afficherMonstres();
+                    dernierDeplacement = now;
+                }
             }
         };
+
         gameLoop.start();
     }
 
     private void creerSprite(Monstre monstre) {
-        Image image = null;
-        if (monstre instanceof Zombie) {
-            image = new Image(Main.class.getResourceAsStream("Monstres/zombie.png"));
-        }
-        else if(monstre instanceof Araignee){
-            image = new Image(Main.class.getResourceAsStream("Monstres/araignee.png"));
+        Image image = imagePourMonstre(monstre);
+
+        if (image == null) {
+            System.out.println("Image introuvable pour : " + monstre.getClass().getSimpleName());
+            return;
         }
 
-        else if(monstre instanceof Squelette){
-            image = new Image(Main.class.getResourceAsStream("Monstres/squelette.png"));
-        }
-
-        else if(monstre instanceof Pillager){
-            image = new Image(Main.class.getResourceAsStream("Monstres/pillager.png"));
-        }
-
-        else if(monstre instanceof Boss){
-            image = new Image(Main.class.getResourceAsStream("Monstres/boss.png"));
-        }
         ImageView sprite = new ImageView(image);
-
         sprite.setId("monstre");
 
-        sprite.setFitWidth(64);
-        sprite.setFitHeight(64);
+        sprite.setFitWidth(TAILLE_TUILE);
+        sprite.setFitHeight(TAILLE_TUILE);
 
-        sprite.setTranslateX(monstre.getX());
-        sprite.setTranslateY(monstre.getY());
+        Position position = monstre.getPosition();
 
-        paneCarte.getChildren().add(sprite);
+        sprite.setLayoutX(position.getColonne() * TAILLE_TUILE);
+        sprite.setLayoutY(position.getLigne() * TAILLE_TUILE);
+
+        paneSprites.getChildren().add(sprite);
+    }
+
+    private Image imagePourMonstre(Monstre monstre) {
+        if (monstre instanceof Zombie) {
+            return new Image(Main.class.getResourceAsStream("Monstres/zombie.png"));
+        }
+
+        if (monstre instanceof Araignee) {
+            return new Image(Main.class.getResourceAsStream("Monstres/araignee.png"));
+        }
+
+        if (monstre instanceof Squelette) {
+            return new Image(Main.class.getResourceAsStream("Monstres/squelettes.png"));
+        }
+
+        if (monstre instanceof Pillager) {
+            return new Image(Main.class.getResourceAsStream("Monstres/pillager.png"));
+        }
+
+        if (monstre instanceof Boss) {
+            return new Image(Main.class.getResourceAsStream("Monstres/boss.png"));
+        }
+
+        return null;
     }
 
     private void afficherMonstres() {
-        paneCarte.getChildren().removeIf(node -> "monstre".equals(node.getId()));
+        paneSprites.getChildren().clear();
 
-        paneCarte.getChildren().removeIf(node ->
-                "monstre".equals(node.getId())
-        );
-        for(Monstre m : partie.getMonstres()){
+        for (Monstre m : partie.getMonstres()) {
             creerSprite(m);
         }
     }
--- src/main/java/universite_paris8/iut/ademir/demo1/Modele/Jeu/Partie.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Modele/Jeu/Partie.java
@@ -1,5 +1,6 @@
 package universite_paris8.iut.ademir.demo1.Modele.Jeu;
 
+import universite_paris8.iut.ademir.demo1.Modele.Cartes.Position;
 import universite_paris8.iut.ademir.demo1.Modele.Monstres.Monstre;
 import universite_paris8.iut.ademir.demo1.Modele.Monstres.Zombie;
 
@@ -8,16 +9,18 @@ import java.util.ArrayList;
 public class Partie {
 
     private ArrayList<Monstre> monstres;
+    private ArrayList<Position> chemin;
 
-    public Partie() {
-        monstres = new ArrayList<>();
+    public Partie(ArrayList<Position> chemin) {
+        this.monstres = new ArrayList<>();
+        this.chemin = chemin;
     }
 
     public void ajouterZombie() {
-        monstres.add(new Zombie());
+        monstres.add(new Zombie(chemin));
     }
 
-    public void MettreAJour() {
+    public void mettreAJour() {
         for (Monstre m : monstres) {
             m.avancer();
         }
